9ec6d0c2bc824bc36bb2432484960184b345badd
Lucas Favaro <paradigma0621@gmail.com>
2026-08-24 19:50:36 -0300

    04 - macbook

--- atv1/macbook.sh
+++ atv1/macbook.sh
@@ -1,3 +1,1 @@
-### k3s-macbook  (obs: preencher authkey com token tskey-auth-xxxx)
-curl -fsSL https://tailscale.com/install.sh | sh
-tailscale up --authkey=tskey-auth-kXFMjFWDfE11CNTRL-txL4WKHJNsTWZU3VM68ssTvudp7TPxp7L --hostname=k3s-macbook
+/usr/local/bin/k3s-agent-uninstall.sh
