6dd6d191b16d79a207636d67c6f1978dbd3bf122
Lucas Favaro <paradigma0621@gmail.com>
2026-08-24 19:42:00 -0300

    +macbook.sh

---
+++ atv1/macbook.sh
@@ -0,0 +1,3 @@
+### k3s-macbook  (obs: preencher authkey com token tskey-auth-xxxx)
+curl -fsSL https://tailscale.com/install.sh | sh
+tailscale up --authkey=tskey-auth-k6Xbo8ZCur11CNTRL-zvsxSdpVbUKpsnArRSwiUKZM5fXJPQvm1 --hostname=k3s-macbook
