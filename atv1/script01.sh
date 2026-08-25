cfcd9c71fed9d6674560225f45ee128d216d0d38
Lucas Favaro <paradigma0621@gmail.com>
2026-08-25 20:19:52 -0300

    Adições

--- atv1/script01.sh
+++
@@ -1,2 +0,0 @@
-curl -fsSL https://tailscale.com/install.sh | sh
-tailscale up --authkey=tskey-auth-kAtayQuXov11CNTRL-o8G8YhH6NPgXQjmpu2JGPg4yP7kfc1BD --hostname=k3s-macbook 
--- ok.sh
+++
@@ -1,5 +0,0 @@
-curl -sfL https://get.k3s.io | \
-  K3S_URL="https://k3s-server.taild969c2.ts.net:6443" \
-  K3S_TOKEN="K101f512735ecbfb4445f040e46eed37b58937fe75a703c2c0a3004f63d8ecc1bd::server:0ec76ab61efa999c30775058e9dbbd5" \
-   sh -s - agent \
-   --vpn-auth="name=tailscale,joinKey=tskey-auth-k2Qhp9ipUy11CNTRL-SZxdFu1Y9HdcLbY18zhKHd9fqvzfZcd"
